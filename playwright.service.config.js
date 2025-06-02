4673078d6ab0d1e3ea9017e89f82e0a87ca3d544
Warren Kay De Leon <warren.kay.g.de.leon@gmail.com>
2025-06-02 20:10:22 +0800

    with azure

---
+++ playwright.service.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require('@playwright/test');
+const { getServiceConfig, ServiceOS } = require('@azure/microsoft-playwright-testing');
+const config = require('./playwright.config');
+
+/* Learn more about service configuration at https://aka.ms/mpt/config */
+export default defineConfig(
+  config,
+  getServiceConfig(config, {
+    exposeNetwork: '<loopback>',
+    timeout: 30000,
+    os: ServiceOS.LINUX,
+    useCloudHostedBrowsers: true // Set to false if you want to only use reporting and not cloud hosted browsers
+  }),
+  {
+    /* 
+    Playwright Testing service reporter is added by default.
+    This will override any reporter options specified in the base playwright config.
+    If you are using more reporters, please update your configuration accordingly.
+    */
+    reporter: [['list'], ['@azure/microsoft-playwright-testing/reporter']],
+  }
+);
